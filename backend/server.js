31c519814b90c0e135e4e47f21b497d46d49fba7
unknown <praneethshetty626@gmail.com>
2022-07-16 05:18:51 +0530

    final-commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 // MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/jobPortal", {
+  .connect("mongodb://localhost:27017/new-jobPortal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
